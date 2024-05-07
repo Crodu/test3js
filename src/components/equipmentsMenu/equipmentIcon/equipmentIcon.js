4cdf91f81a3211b09a6baf1f4598713ef668bf97
Crodu <soratto.andre@gmail.com>
2024-05-07 19:22:47 -0300

    change tractor path

--- src/components/equipmentsMenu/equipmentIcon/equipmentIcon.js
+++ src/components/equipmentsMenu/equipmentIcon/equipmentIcon.js
@@ -2,7 +2,7 @@ import React, { forwardRef } from 'react';
 import { useGLTF } from '@react-three/drei';
 
 const EquipmentIcon = (props, ref) => {
-  const gltf = useGLTF('/tractor.gltf');
+  const gltf = useGLTF('/threejs_terrain_tools/tractor.gltf');
 
   return (
     <mesh raycast={() => null} visible={false} ref={ref} {...props}>
